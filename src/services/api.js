3f759a66d8bdb7a4b9185747f0578a3db4df170d
Rishith-21 <shettyrishith2005@gmail.com>
2026-09-20 19:51:35 +0530

    Ready to deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "http://52.66.121.243:5000/api",
 });
 
 API.interceptors.request.use((config) => {
