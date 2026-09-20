acc9593f8412b0197f603f87e7c0ab8065e895ce
Rishith-21 <shettyrishith2005@gmail.com>
2026-09-20 21:05:09 +0530

    Ready to deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: "http://autoaxis.duckdns.org/api",
+    baseURL: "https://autoaxis.duckdns.org/api",
 });
 
 API.interceptors.request.use((config) => {
